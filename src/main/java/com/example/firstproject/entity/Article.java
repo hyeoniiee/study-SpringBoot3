0fa2c045011f519dff5d6c3051eb4b8e002ddf8a
hyeoniiee <starjea0126@gmail.com>
2024-10-21 18:14:48 +0900

    11일차 - 11장 REST API 구현

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,7 +14,7 @@ import lombok.ToString;
 public class Article {
 
     @Id // 엔티티의 대표값 지정
-    @GeneratedValue // 자동 생성 기능 추가 (숫자가 자동으로 매겨짐)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 생성 기능 추가 (숫자가 자동으로 매겨짐)
     private Long id;
 
     @Column
@@ -26,4 +23,12 @@ public class Article {
     @Column
     private String content;
 
+    public void patch(Article article) {
+        if (article.title != null) {
+            this.title = article.title;
+        }
+        if (article.content != null) {
+            this.content = article.content;
+        }
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
-INSERT INTO article(id, title, content) VALUES(1, 'aaaa', '1111');
-INSERT INTO article(id, title, content) VALUES(2, 'bbbb', '2222');
-INSERT INTO article(id, title, content) VALUES(3, 'cccc', '3333');
+-- 11장 셀프체크 데이터 추가
+INSERT INTO coffee(name, price) VALUES('아메리카노', '4500');
+INSERT INTO coffee(name, price) VALUES('라떼', '5000');
+INSERT INTO coffee(name, price) VALUES('카페 모카', '5500');
