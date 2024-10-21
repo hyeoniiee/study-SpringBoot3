1e4b536e266b5dee0ea6030febbe720894702fca
hyeoniiee <starjea0126@gmail.com>
2024-10-22 02:53:15 +0900

    14일차 - 14장 댓글 리파지토리 만들기

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,13 +1,10 @@
 package com.example.firstproject.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Entity
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
